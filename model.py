af3d3fee22636c0451350ee2b8d3612642d4a019
Gaja Jamnik <gaja.jamnik@gmail.com>
2020-05-13 15:29:10 +0200

    Dodan model

---
+++ model.py
@@ -0,0 +1,73 @@
+STEVILO_DOVOLJENIH_NAPAK = 10
+
+PRAVILNA_CRKA = '+'
+PONOVLJENA_CRKA = 'o'
+NAPACNA_CRKA = '-'
+
+ZMAGA = 'W'
+PORAZ = 'X'
+
+import random
+
+class Igra:
+    def __init__(self, geslo, crke=None):
+        self.geslo = geslo.upper()
+        self.crke = [] if crke is None else [crka.upper() for crka in crke]
+
+    def napacne_crke(self):
+        return [crka for crka in self.crke if crka not in self.geslo]
+
+    def pravilne_crke(self):
+        return [crka for crka in self.crke if crka in self.geslo]
+
+    def stevilo_napak(self):
+        return len(self.napacne_crke())
+
+    def zmaga(self):
+        return set(self.geslo) == set(self.pravilne_crke())
+        #2.MOZNOST:
+        #return all(crkain self.crke for crka in self.geslo)
+        #3.MOZNOST:
+        #for crka in self.geslo:
+        #    if crka not in self.pravilne_crke:
+        #        return False
+        #return True
+
+    def poraz(self):
+        return self.stevilo_napak() >= STEVILO_DOVOLJENIH_NAPAK
+
+    def pravilni_del_gesla(self):
+        izhodni_niz = ''
+        for crka in self.geslo:
+            izhodni_niz += crka if crka in self.crke else '_'
+        return izhodni_niz
+
+    def nepravilni_ugibi(self):
+        return ' '.join(self.napacne_crke())
+
+    def ugibaj(self, ugibana_crka):
+        ugibana_crka = ugibana_crka.upper()
+        if ugibana_crka in self.crke():
+            return PONOVLJENA_CRKA
+        else:
+            self.crke.append(ugibana_crka)
+            if ugibana_crka in self.geslo:
+                if self.zmaga():
+                    return ZMAGA
+                else:
+                    return PRAVILNA_CRKA
+            else:
+                if self.poraz():
+                    return PORAZ
+                else:
+                    return NAPACNA_CRKA
+
+bazen_besed = []
+for beseda in open('besede.txt', encoding='utf-8'):
+    bazen_besed.append(beseda.strip().upper())
+
+def nova_igra():
+    beseda = random.choice(bazen_besed)
+    #metoda random.choice vrne nakoljucno izbrano besedo iz bazena besed
+    #rabis pa zato se na vrhi importat
+    return Igra(beseda)
